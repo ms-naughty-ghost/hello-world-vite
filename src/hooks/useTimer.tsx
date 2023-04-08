0eaf8a3c15c5076b9eea5e7fe8516d77b9a9d160
ms.naughty.ghost@gmail.com <ms.naughty.ghost@gmail.com>
2023-04-08 17:38:43 +0900

    useRefサンプル

--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -1,22 +1,39 @@
 // タイマーフック
-import { useState, useEffect, useMemo } from 'react';
+import {
+  useState,
+  useEffect,
+  useMemo,
+  useRef,
+  useCallback,
+  SyntheticEvent,
+} from 'react';
 import { getPrimeNumbers } from '../utils/prime';
 
 export const useTimer = (maxCount: number): [number, boolean, () => void] => {
   const [count, setCount] = useState(maxCount);
   const primes = useMemo(() => getPrimeNumbers(maxCount), [maxCount]);
+  // タイマーID
+  const timerId = useRef<number>();
+
   // タイマーのカウントダウンを行う関数
-  const tick = () => setCount(count - 1);
+  const tick = useCallback(() => setCount((t) => t - 1), []);
   // タイマーのリセットを行う関数
-  const reset = () => setCount(maxCount);
+  const reset = useCallback(
+    (event?: SyntheticEvent) => {
+      event?.stopPropagation();
+      if (timerId.current) clearTimeout(timerId.current);
+      setCount(maxCount);
+      timerId.current = setTimeout(tick, 1000);
+    },
+    [maxCount, tick]
+  );
 
   // タイマーのカウントダウンを行う
   useEffect(() => {
-    const timer = setTimeout(tick, 1000);
+    timerId.current = setTimeout(tick, 1000);
 
-    return () => clearTimeout(timer);
+    return () => clearTimeout(timerId.current!);
   }, [count]);
-
   // countが0以下になったらリセットする
   useEffect(() => {
     if (count <= 0) {
